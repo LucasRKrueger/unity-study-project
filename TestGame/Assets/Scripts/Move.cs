643f811e7d705fe665102336abf070c4b3564b9f
LucasRKrueger <lucasrkthd@gmail.com>
2020-05-20 22:45:22 -0300

    firs commit

---
+++ TestGame/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float _moveSpeed = 5.0f;
+    void Start()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * _moveSpeed;
+    }
+ 
+    void Update()
+    {
+        Movement();
+    }
+
+    bool isMoving()
+    {
+        return new Rigidbody2D().velocity.magnitude > 0;
+    }
+}
